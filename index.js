228075e711918ec1a83a4cbf02fa3a5b70c937ab
Soham Sachin Pirale <162886548+sohampirale@users.noreply.github.com>
2025-08-14 23:14:52 +0530

    4th change CICD testing

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const app=express();
 app.get('/',(req,res)=>{
   res.status(200).json({
     message:'Hello World',
-    change:"Testing CICD third change"
+    change:"Testing CICD 4th change"
   })
+})
+
+app.get("sui",(req,res)=>{
+  res.send("SUIIII CID wokring!!");
 })
 
 app.listen("3000",()=>{
